2dfaca47cfb7dce1c06acf8448548a6872edf13b
illenko <kostiaillenko@gmail.com>
2024-12-25 17:21:45 +0200

    Add prometheus metrics for outbox-processor

--- outbox-processor/main.go
+++ outbox-processor/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type OutboxMessage struct {
@@ -15,36 +19,53 @@ type OutboxMessage struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+var (
+	messagesProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "messages_processed_total",
+		Help: "The total number of processed messages",
+	})
+	messagesFailed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "messages_failed_total",
+		Help: "The total number of failed messages",
+	})
+)
+
 func main() {
-	dbpool, err := getPool()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dbpool.Close()
 
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
+	go func() {
+		dbpool, err := getPool()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer dbpool.Close()
 
-	batchSize := 1000
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
 
-	for {
-		select {
-		case <-ticker.C:
-			if err := processMessages(dbpool, batchSize); err != nil {
-				log.Printf("Error processing messages: %v", err)
+		batchSize := 5
+		for {
+			select {
+			case <-ticker.C:
+				start := time.Now()
+				if err := processMessages(dbpool, batchSize); err != nil {
+					log.Printf("Error processing messages: %v", err)
+				}
+				elapsed := time.Since(start)
+				log.Printf("processMessages took %s", elapsed)
 			}
 		}
-	}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8081", nil)
 }
 
 func processMessages(dbpool *pgxpool.Pool, limit int) error {
-	log.Println("Processing outbox messages")
-
 	ctx := context.Background()
 
 	tx, err := dbpool.Begin(ctx)
 	if err != nil {
-		log.Fatalf("Error starting transaction: %v", err)
+		log.Printf("Error starting transaction: %v", err)
 		return err
 	}
 	defer tx.Rollback(ctx)
@@ -54,7 +75,7 @@ func processMessages(dbpool *pgxpool.Pool, limit int) error {
 	rows, err := tx.Query(ctx, query, limit)
 
 	if err != nil {
-		log.Fatalf("Error querying outbox messages: %v", err)
+		log.Printf("Error querying outbox messages: %v", err)
 		return err
 	}
 	defer rows.Close()
@@ -63,7 +84,7 @@ func processMessages(dbpool *pgxpool.Pool, limit int) error {
 	for rows.Next() {
 		var outboxMessage OutboxMessage
 		if err := rows.Scan(&outboxMessage.ID, &outboxMessage.EntityID, &outboxMessage.Payload, &outboxMessage.CreatedAt); err != nil {
-			log.Fatalf("Error scanning outbox message: %v", err)
+			log.Printf("Error scanning outbox message: %v", err)
 			return err
 		}
 		messages = append(messages, outboxMessage)
@@ -82,9 +103,13 @@ func processMessages(dbpool *pgxpool.Pool, limit int) error {
 	}
 
 	if err := tx.Commit(ctx); err != nil {
-		log.Fatalf("Error committing transaction: %v", err)
+		log.Printf("Error committing transaction: %v", err)
+
+		messagesFailed.Add(float64(len(messages)))
 		return err
 	}
+
+	messagesProcessed.Add(float64(len(messages)))
 
 	return nil
 }
